59aa6fe7a804d63f04c6158efb7922654b902532
Muhammad Hasan <muhammad.hasann845@gmail.com>
2025-09-25 02:45:59 +0500

    Add files via upload

---
+++ Labs/Lab 01 - Getting Started/task01.java
@@ -0,0 +1,19 @@
+/*
+    Create a new java project on NetBeans named as “First_Exercise”. Display a text message saying, “this is my first program”. Provide multiline document string for the created program explaining the process in the program
+ */
+
+package Tasks;
+
+/*
+    - this is my first task of lab 01
+    - first lab od second semester
+    - this lab is about getting started with netbeans
+ */
+
+
+public class task01 {
+    public static void main(String[] args) {
+
+        System.out.println("This is my first program.");
+    }
+}
---
+++ Labs/Lab 01 - Getting Started/task02.java
@@ -0,0 +1,16 @@
+/*
+    Create a java program named “Second_Exercise”. Print the name, enrollment, Subject name, Instructor name and semester of  a student and attach proper output screenshot. Provide proper comments.
+ */
+
+package Tasks;
+
+public class task02 {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Muhammad Hasan");
+        System.out.println("Enrollment: 02-135251-040");
+        System.out.println("Course: Object Oriented Programming Lab");
+        System.out.println("Instructor: Madiha Aslam");
+        System.out.println("Semester: BSIT-2 (Fall 2025)");
+    }
+}
